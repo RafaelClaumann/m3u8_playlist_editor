e9d39a6cd736d311f378f5c9a0b69041e4d4fffc
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-07-11 19:42:54 -0300

    removing unwanted groups

--- main.py
+++ main.py
@@ -13,11 +13,27 @@ def remove_low_quality_channels(channels: list):
         else:
             i += 1
 
+def remove_unwanted_groups(channels: list, groups: list):
+    group_pattern = r'group-title="{}"'
+    i = 0
+    while i < len(channels):
+        if channels[i].startswith("#EXTINF:"):
+            for group in groups:
+                if re.search(group_pattern.format(re.escape(group)), channels[i]):
+                    del channels[i]
+                    del channels[i]
+            i += 1  
+        else:
+            i += 1
+
 def main():
     f = open("sample_playlist.m3u8", "r")
+    
     channels = f.read().split('\n')
+    unwanted_groups = ["NOTICIAS"]
 
     remove_low_quality_channels(channels=channels)
+    remove_unwanted_groups(channels=channels, groups=unwanted_groups)
 
     f = open("output_playlist.m3u8", "w")
     f.write("\n".join(channels))
