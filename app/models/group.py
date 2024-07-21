0135138a1fd1fa7972f1e43a64381507a68beac4
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-07-20 21:51:27 -0300

    Add group_type attribute and update __repr__ method in Group class

--- app/models/group.py
+++ app/models/group.py
@@ -1,11 +1,13 @@
 class Group:
     def __init__(self,
+                 group_type: str = None,
                  tvg_group: str = None,
                  tvg_names: list = None,
                  first_occurrence: int = None,
                  last_occurrence: int = None,
                  total_occurrences: int = None
                  ):
+        self.group_type = group_type
         self.tvg_group = tvg_group
         self.tvg_names = tvg_names if tvg_names is not None else []
         self.first_occurrence = first_occurrence
@@ -13,7 +15,8 @@ class Group:
         self.total_occurrences = total_occurrences
 
     def __repr__(self):
-        return (f"Media("
+        return (f"Group("
+                f"group_type={self.group_type},"
                 f"tvg_group={self.tvg_group},"
                 f"tvg_names={self.tvg_names},"
                 f"first_occurrence={self.first_occurrence},"
